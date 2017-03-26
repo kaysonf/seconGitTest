3b7b0d4c8410037883ed7c651f10261adb63c95f
kayson Fu <kaysonf@hotmail.com>
2017-03-26 13:46:45 +0800

    Initial Commit

---
+++ secondGitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondGitTest
+//
+//  Created by kayson fu on 26/3/17.
+//  Copyright © 2017 kayson fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
